69b708351cbfef4624a0d0e49053eccf97aa26c6
Oliver O'Brien <o.obrien@ucl.ac.uk>
2020-07-18 18:44:52 +0100

    Input validation

--- www/utf_tilefilewrapper.php
+++ www/utf_tilefilewrapper.php
@@ -7,6 +7,11 @@ $y = htmlspecialchars($_GET['y'], ENT_QUOTES);
 $x = htmlspecialchars($_GET['x'], ENT_QUOTES);
 $json_name = htmlspecialchars($_GET['json_name'], ENT_QUOTES);
 
+if (!is_numeric($z)) { return; }
+if (!is_numeric($y)) { return; }
+if (!is_numeric($x)) { return; }
+if (strlen($json_name) > 20) { return; }
+
 echo $callback . "(";
 $file = file_get_contents(/* Your server here */"/$json_name/$z/$x/$y.json");
 echo $file;
